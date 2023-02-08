be9b2c865291bba3222070572de36e88e40678d4
DawidKrupski <dawid.krupski323@gmail.com>
2023-02-08 22:53:07 +0100

    update

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,14 +1,14 @@
-// import React from 'react';
+import React from 'react';
 // import PropTypes from 'prop-types';
 
-// export class ContactList extends React.Component {
-//   render() {
-//     const contact = this.props;
-//     return (
-//       <li>
-//         {contact.contact.name}: {contact.contact.number}
-//         {/* <button> Delete</button> */}
-//       </li>
-//     );
-//   }
-// }
+export class ContactList extends React.Component {
+  render() {
+    const contact = this.props;
+    return (
+      <li>
+        {contact.contact.name}: {contact.contact.number}
+        {/* <button> Delete</button> */}
+      </li>
+    );
+  }
+}
