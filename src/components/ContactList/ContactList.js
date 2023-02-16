ba97773451635df1468a70175dad47820bf0fe6a
DawidKrupski <dawid.krupski323@gmail.com>
2023-02-16 20:34:31 +0100

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,12 +2,19 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 export class ContactList extends React.Component {
+  deleteThisContact = () => {
+    const { contact, deleteContact } = this.props;
+    const { id } = contact;
+
+    deleteContact(id);
+  };
+
   render() {
     const { contact, deleteContact } = this.props;
     return (
       <li>
         {contact.name}: {contact.number}
-        <button onClick={this.deleteContact}>Delete</button>
+        <button onClick={deleteContact}>Delete</button>
       </li>
     );
   }
